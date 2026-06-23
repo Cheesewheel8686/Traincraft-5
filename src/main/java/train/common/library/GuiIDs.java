0a7c9ebb7182b7b200c611ce872c68f1e1da1fb6
TBEA <74006945+RealTBEA@users.noreply.github.com>
2026-06-22 23:30:05 -0400

    Update GuiIDs.java

--- src/main/java/train/common/library/GuiIDs.java
+++ src/main/java/train/common/library/GuiIDs.java
@@ -36,4 +36,5 @@ public class GuiIDs {
     public static final int DYNAMIC_OVERLAY = 114;
     public static final int OVERLAY_MENU = 115;
 }    public static final int LOCKOUT_BOOK = 116;
+    public static final int LOCKOUT_BOOK = 116;
 }
